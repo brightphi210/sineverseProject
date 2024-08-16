140b86bdd3145e729b634200a241d84f88d1c2f6
Bright Philip <chibuzorphilip2001@gmail.com>
2024-08-16 13:42:35 +0100

    Latest

--- sineverseProject/settings.py
+++ sineverseProject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-isqqlzkuy4m%+ktirhdq1)y-#otqcv$@@-bua*%gm+-cjoh8aa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
