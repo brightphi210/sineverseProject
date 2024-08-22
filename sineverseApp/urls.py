0874c18f3efd20a17d5e6042e32d5588ad1dadf6
Bright Philip <chibuzorphilip2001@gmail.com>
2024-08-22 17:37:06 +0100

    Latest

--- sineverseApp/urls.py
+++ sineverseApp/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('silver_coin/<str:tgID>/', SilverCoinsView.as_view(), name='silver_coin'),
     # path('silver_coin/update/<str:pk>/', SilverCoinsViewUpdate.as_view(), name='gold_coin'),
     
-    path('daily_reward/', DailyRewardView.as_view(), name='daily_reward'),
+    # path('daily_reward/', DailyRewardView.as_view(), name='daily_reward'),
     # path('daily_reward/update/<str:pk>/', DailyRewardView.as_view(), name='daily_reward'),
     path('claim_reward/', ClaimRewardView.as_view(), name='claim-reward'),
 
